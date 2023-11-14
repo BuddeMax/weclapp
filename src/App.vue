e7fe3ff8eb698f8d8c0c13c7a2cd4d94982a6128
BuddeMax <Max.Budde@Student.HTW-Berlin.de>
2023-11-14 09:45:07 +0100

    Hello World

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Patient />
+  </div>
+</template>
+
+<script>
+import Patient from './components/Patient.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Patient
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
